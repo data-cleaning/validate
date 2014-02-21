8c3940a733520bacffead57f363c7e65ec4ac11f
mark <mark.vanderloo@gmail.com>
2014-02-21 17:06:33 +0100

    object hierarchy

---
+++ pkg/R/generic.R
@@ -0,0 +1,72 @@
+
+#  Superclass for storing validation rules.
+setRefClass("validator"
+  , fields = list(calls = 'list',origin= 'character')
+  , methods= list(
+    initialize = function(...,files=NULL) .validator(.self,...,files=files)
+    ,show = function() .show_validator(.self) 
+    
+    )
+)
+
+# confront data with a subclass of 'validator'
+setGeneric("confront",
+  def = function(x, ...) standardGeneric("confront")
+)
+
+
+# IMPLEMENTATIONS -------------------------------------------------------------
+
+.validator <- function(.self, ..., files){
+  L <- as.list(substitute(list(...))[-1])
+
+  if ( !is.null(file) && is.character(file) ){
+    L <- list()
+    ifile <- character(0)
+    for ( f in file ){ 
+      L <- c(L,read_resfile(f))
+      ifile <- c(ifile,rep(f,length(L)))
+    }
+  } else if (length(L)==0){
+    return(.self)
+  } else {
+    names(L) <- extract_names(L)
+    ifile <- rep("commandline",length(L))
+  }
+  .self$calls <- L
+  .self$origin <- ifile
+}
+
+# get names from a list, replacing empty names values with numbers
+extract_names <- function(L){
+  generic <- sprintf("%04d",1:length(L))
+  given <- names(L)
+  if (is.null(given)) return(generic)
+  igen <- given == ""
+  given[igen] <- generic[igen]
+  given
+}
+
+
+.show_validator <- function(.self){
+  nr <- length(.self$calls)
+  cat(sprintf(
+    "Reference object of class '%s' with %s elements\n",class(.self)[1], nr
+  ))
+  if (nr == 0) return(invisible(NULL))
+  lab <- names(.self$calls)
+  n <- max(nchar(lab))
+  lab <- paste0(format(lab,width=n),": ",sapply(.self$calls, call2text))
+  cat(noquote(paste(lab,collapse="\n")))
+  cat("\n")
+}
+
+# from call to oneliner text
+call2text <- function(x){
+  gsub("[[:blank:]]+"," ",paste(deparse(x),collapse=" "))
+}
+
+
+
+
+
--- pkg/R/variable.R
+++ pkg/R/variable.R
@@ -1,62 +1,40 @@
 # The 'indicator' class holds indicator definitions
 # An indicator maps a data.frame to a single number.
 indicator <- setRefClass("indicator"
-  , fields = list(ind='list',origin='character')
+  , contains='validator'
   , methods= list(
-    initialize = function(...,files=NULL) .indicator(.self, ..., files=files)
-    , show = function() .show_ind(.self)
-    , compute = function(...) .compute_indicators(.self,...)
+     expand = function(vars=NULL) .expand(.self, vars)
     )
 )
 
 
-.indicator <- function(.self,...,files){
-  .self$ind <- as.list(substitute(list(...))[-1])
-  names(.self$ind) <- extract_names(.self$ind)
-}
-
-.show_ind <- function(.self){
-  nr <- length(.self$ind)
-  cat(sprintf(
-    "Reference object of class 'indicator' with %s indicators\n", nr
-  ))
-  if (nr == 0) return(invisible(NULL))
-  lab <- names(.self$ind)
-  n <- max(nchar(lab))
-  lab <- paste0(format(lab,width=n),": ",sapply(.self$ind,deparse))
-  cat(noquote(paste(lab,collapse="\n")))
-  cat("\n\n")
-}
+setMethod('confront',signature=c(x='indicator'),
+  function(x,...) .compute_indicators(x,...)
+)
 
 
-.compute_indicators <- function(.self,...){
-  D <- list(...)
-  L <- unlist(lapply(.self$ind,expand,names(D[[1]])))
-  sapply(D,function(d) sapply(L,eval,d))
+# IMPLEMENTATIONS -------------------------------------------------------------
+
+.expand <- function(.self,vars){
+  L <- lapply(.self$calls,expand,vars)
+  .self$origin <- unlist(lapply(seq_along(L), function(i) rep(.self$origin[i],length(L[[i]]))))
+  .self$calls <- unlist(L)
 }
 
-# get names from a list, replacing empty names values with numbers
-extract_names <- function(L){
-  generic <- sprintf("Ind%04d",1:length(L))
-  given <- names(L)
-  if (is.null(given)) return(generic)
-  igen <- given == ""
-  given[igen] <- generic[igen]
-  given
-}
 
 # a little syntactic sugar to apply functions to multiple variables.
 # x : a call object
 # vars : character vector
 expand <- function(x,vars=NULL){
   M <- x
-  vars <- sapply(vars,as.symbol)
+  if (!is.null(vars)) vars <- sapply(vars,as.symbol)
   for ( i in 1:length(x))
     if (x[[i]] == '{'){
       if ( length(x[[i]]) > 1 ){ # variables specified as {<x>; <y>; ...}
         M <- lapply(2:length(x[[i]]), function(i) x )
         for ( j in 2:length(x[[i]]) ) M[[j-1]][[i]] <- x[[i]][[j]]
       } else { # all variables
+        if (is.null(vars)) stop('You need to provide variable names for indicators with unspecified variables')
         M <- lapply(seq_along(vars),function(i) x)
         for ( j in seq_along(M) ) M[[j]][[i]] <- vars[[j]]
       }
@@ -65,17 +43,31 @@ expand <- function(x,vars=NULL){
 }
 
 
+.compute_indicators <- function(x,...){
+  D <- list(...)
+  L <- unlist(lapply(x$calls,expand,names(D[[1]])))
+  sapply(D,function(d) sapply(L,eval,d))
+}
+
 
 # Example
  data(women)
 # specify variables
 ind <- indicator(gem=mean({weight; height}, na.rm=TRUE),sd(weight))
 
-ind$compute(w1=women)
-ind$compute(w1=women, w2=2*women)
+confront(ind,w1=women)
+confront(ind,w1=women, w2=2*women)
+ind
+ind$expand()
+ind
+
+
 
 # all variables
 ind2 <- indicator(gem=mean({},na.rm=TRUE))
-ind2$compute(w=women,w0=2*women)
+confront(ind,w=women,w0=2*women)
+ind2
+ind2$expand(names(women))
+ind2
 
 
