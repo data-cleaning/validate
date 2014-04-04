ba37c60e928a965734432aa7bba6440d00af32d5
mark <mark.vanderloo@gmail.com>
2014-04-04 09:18:13 +0200

    addded 'fraction_missing' function

--- pkg/R/functions.R
+++ pkg/R/functions.R
@@ -1,15 +1,19 @@
 # functions, added to the syntax of validator and indicator objects
 
-#' Count number of missing values
+#' Count (fraction of) missing values
 #' 
 #' This function should not be called directly, but only be used as syntax when
 #' defining an \code{\link{indicator}}.
 #' 
-#' @param ... comma-separated list of variable names (not character) 
+#' @param ... comma-separated list of variable names (not character). If no
+#'  variables are specified, the number of missings over all data is counted.
+#'  
+#' @return The total number of missings over all specified variables.
+#'  
 #' @export
 number_missing <- function(...){
   L <- as.list(substitute(list(...))[-1])
-  vars <- if( length(L) == 0 ) ls() else sapply(L,as.character)
+  vars <- if( length(L) == 0 ) TRUE else sapply(L,as.character)
   sum(sapply(
     eapply(
       env=parent.frame()
@@ -18,10 +22,28 @@ number_missing <- function(...){
   ,Id))
 }
 
+#' @rdname number_missing
+fraction_missing <- function(...){
+  L <- as.list(substitute(list(...))[-1])
+  vars <- if( length(L) == 0 ) TRUE else sapply(L,as.character)
+  v <- sapply(
+    eapply(
+      env=parent.frame()
+      , FUN = function(x) c(sum(is.na(x)),length(x)) #tryCatch(sum(is.na(x)), warning=function(e) 0)
+    )[vars]
+    ,Id)
+  sum(v[1,])/sum(v[2,])
+}
+
+
+
+
+
 
 # d <- data.frame(
 #   x = c(1,NA,3,5)
 #   , y = c(NA,NA,1,2)
 #   , z = letters[1:4]
 #   )
-# eval(quote(number_missing(x,y)),envir = d)
+# I <- indicator(fraction_missing(x),number_missing(),fraction_missing(x,z))
+# values(confront(I,d))
