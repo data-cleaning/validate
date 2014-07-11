e50566e6f1f24fb8dd40e56f2c2e3ff39cbc8e87
mark <mark.vanderloo@gmail.com>
2014-07-11 21:14:19 +0200

    added retailers dataset

---
+++ pkg/R/retailers.R
@@ -0,0 +1,7 @@
+#' @name retailers
+#' @title data on Dutch retailers
+#' @description Data on revenue and cost structure for 60 retailers
+#' @docType data
+#' @format A csv file, one retailer per row.
+NULL
+
