e6af9892a84ec6fce9ea10aa2628e6cd611acc61
mark <mark.vanderloo@gmail.com>
2015-07-29 14:21:58 +0200

    added drat deployer

---
+++ drat.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/Rscript
+
+suppressPackageStartupMessages({
+  if (!require("drat")) stop("drat not installed")
+  if (!require("docopt")) stop("docopt not installed")
+})
+
+ 
+"Usage: drat.sh [commit] [--pkg FILE] [--dratrepo FOLDER] 
+
+commit commit after insert? 
+--pkg FILE The tarball to insert in the drat repo (by default the tarball in ./output)
+--dratrepo FOLDER path to root of drat repo [default: ../drat]
+" -> doc
+
+opt <- docopt(doc)
+
+stopifnot(file.exists(opt$dratrepo))
+
+pkg <- opt$pkg
+if ( is.null(pkg) ){
+  pkg <- dir("output/",pattern = ".*tar\\.gz",full.names = TRUE)
+} 
+
+if (!file.exists(pkg)){
+  stop(sprintf("%s not found"))
+}
+
+drat::insertPackage(pkg, repodir=opt$dratrepo, commit=opt$commit)
+
+cat(sprintf("Inserted %s into %s %s\n"
+  , pkg
+  , opt$dratrepo
+  , if(opt$commit) "and committed" else ""
+))
