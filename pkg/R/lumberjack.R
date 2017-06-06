ac6cef18544218bd7ec96bb2d880389ee6217068
markvanderloo <mark.vanderloo@gmail.com>
2017-06-06 16:31:33 +0200

    lumberjacking

--- pkg/R/lumberjack.R
+++ pkg/R/lumberjack.R
@@ -25,22 +25,13 @@
 #' This logger is suited only for operations that do not change the dimensions 
 #' of the dataset.
 #'
-#' @usage 
-#' lumberjack::start_log(data, log=cell_counts(verbose=TRUE))
-#' lumberjack::dump(data, file="cells.csv",...)
-#' 
-#' @param data A \code{data.frame}
-#' @param log A logging object
-#' @param verbose Toggle logger verbosity
-#' @param file Filename to dump the log to
-#' @param ... Parameters passed to \code{\link{write.csv}}
-#'
 #' @docType class
 #' @format A reference class object
 #'
 #'
-#' @export
-cell_counts <- setRefClass("lumberjack.cells"
+#' @export cell_counts
+#' @exportClass cell_counts
+cell_counts <- setRefClass("cell_counts"
     , fields = list(
         cells = "array"
         , n     = "numeric"
@@ -49,13 +40,18 @@ cell_counts <- setRefClass("lumberjack.cells"
         , verbose = "logical"
         )
     , methods = list(
-      initialize = function(verbose=FALSE){
+      initialize = function(..., verbose=FALSE){
+        "Create object. Optionally toggle verbosity."
         .self$n <- 0
         .self$t <- .POSIXct(numeric(0))
         .self$verbose = verbose
+        a <- data.frame(g=0)
+        m <- cells(a=a, b=a)
+        .self$cells <- m[, 0, drop=FALSE]
       }
       , add = function(meta, input, output){
-        cl <- cells(input, output)
+        "Add logging info based on in- and output"
+        cl <- cells(from = input, to = output)
         tm <- as.POSIXct(Sys.time())
         if ( .self$n == 0 ){
           .self$cells <- cl[,1,drop=FALSE]
@@ -65,19 +61,30 @@ cell_counts <- setRefClass("lumberjack.cells"
         .self$n <- .self$n+1
         .self$t <- c(.self$t, tm)
         .self$expr <- c(.self$expr, meta$src)
-        .self$cells <- cbind(.self$cells, cl[,2])
+        .self$cells <- cbind(.self$cells, cl[,2,drop=FALSE])
       }
     , dump = function(file="cells.csv",...){
-        out <- data.frame(
-          step = seq(0,.self$n)
-          , time = .self$t
-          , expr = .self$expr
-        )
-        cl <- t(.self$cells)
-        row.names(cl) <- NULL
-        out <- cbind(out, cl)
-        write.csv(out, file=file, row.names=FALSE,...)
-        .self$fmsg("Dumped a log at %s", normalizePath(file))
+      "Dump logging info to csv file. 
+       All arguments in '...' except row.names are passed to 'write.csv'"
+      out <- .self$log_data()
+      write.csv(out, file=file, row.names=FALSE,...)
+      .self$fmsg("Dumped a log at %s", normalizePath(file))
+    }
+    ,  log_data = function(){
+      "Return logged data as a data.frame"
+      out <- data.frame(
+        step = if(.self$n > 0 ) 0:.self$n else integer(0)
+        , time = .self$t
+        , expr = .self$expr
+      )
+      cl <- t(.self$cells)
+      row.names(cl) <- NULL
+      cbind(out, cl)
+    }
+    , show = function(){
+      "Print method"
+      cat("Logging object of class cell_counts\n")
+      print(.self$log_data())
     }
     , fmsg = function(fmt,...){
       if (.self$verbose){
