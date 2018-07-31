b5067ff92a5aba281cabb6b1f8811db398669ab4
Edwin de Jonge <edwindjonge@gmail.com>
2018-07-31 13:54:44 +0200

    improving example cells.R

--- examples/cells.R
+++ examples/cells.R
@@ -11,11 +11,12 @@ step1$turnover[is.na(step1$turnover)] <- mean(step1$turnover,na.rm=TRUE)
 step2 <- step1
 step2$other.rev <- abs(step2$other.rev)
   
-# create an overview of differences compared to raw data
-cells(raw = step0, imputed = step1, flipped = step2)
 
 # create an overview of differences, comparing to the previous step
-out <- cells(raw = step0, imputed = step1, flipped = step2, compare="sequential")
+cells(raw = step0, imputed = step1, flipped = step2, compare="sequential")
+
+# create an overview of differences compared to raw data
+out <- cells(raw = step0, imputed = step1, flipped = step2)
 out
 
 # Graphical overview of the changes
