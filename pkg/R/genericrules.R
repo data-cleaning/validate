19b8573db914baa46e92ec6431032ce36eff7a8a
Mark van der Loo <mark.vanderloo@gmail.com>
2020-05-16 00:07:51 +0200

    edge cases

--- pkg/R/genericrules.R
+++ pkg/R/genericrules.R
@@ -1,20 +1,37 @@
 
-#' Check whether a vector contains a strictly monotone sequence of data
+#' Check whether a vector contains a linear sequence
 #'
-#' A series of data points \eqn{x_1, x_2,\ldots} is a strictly monotone
-#' sequence when \eqn{x_{j+1} - x_j} is constant unequal to zero for all
-#' \eqn{j\geq 0}. In particular, this implies that the series has no gaps and
-#' no duplicates.
+#' A vector of data points \eqn{x_1, x_2,\ldots, x_n} (\eqn{n\geq 0}) is a
+#' \emph{linear sequence} when \eqn{x_{j+1} - x_j} is constant for all
+#' \eqn{j\geq 1}. In particular, this implies that the elements in the series
+#' are equidistant, without gaps. 
+#'
+#' @details
+#' 
+#' Any missing value (\code{NA}) in \code{x} will result in \code{FALSE},
+#' except when \code{length(x) == 1} and \code{start} and \code{end} are
+#' \code{NULL}. (Any sequence of length 1 is a linear sequence). 
+#' 
+#'
 #'
 #' @param x An R vector.
-#' @param sort \code{[logical]} toggle sort before test
+#' @param sort \code{[logical]}. When set to \code{TRUE}, \code{x}
+#'        is sorted before testing.
+#' @param start Optionally, a value that should equal \code{min(x)}
+#' @param end   Optionally, a value that should equal \code{max(x)}
 #' @param ... arguments passed to other methods.
+#'
+#' @return \code{TRUE} or \code{FALSE}.
 #'
 #' @examples
 #'
-#' is_sequence(1:5) # TRUE
-#' is_sequence(c(1,3,5,4,2)) # TRUE
-#' is_sequence(c(1,3,5,4,2), sort=FALSE) # FALSE 
+#' is_linear_sequence(1:5) # TRUE
+#' is_linear_sequence(c(1,3,5,4,2)) # FALSE
+#' is_linear_sequence(c(1,3,5,4,2), sort=TRUE) # TRUE 
+#' is_linear_sequence(NA_integer_) # TRUE
+#' is_linear_sequence(NA_integer_, start=4) # FALSE
+#' is_linear_sequence(c(1, NA, 3)) # FALSE
+#'
 #'
 #' d <- data.frame(
 #'     number = c(pi, exp(1), 7)
@@ -23,34 +40,50 @@
 #' )
 #'
 #' rules <- validator(
-#'     is_sequence(number)  # fails
-#'   , is_sequence(date)    # passes
-#'   , is_sequence(time)    # fails
+#'     is_linear_sequence(number)  # fails
+#'   , is_linear_sequence(date)    # passes
+#'   , is_linear_sequence(time)    # fails
 #' )
 #' summary(confront(d,rules))
 #'
 #' @export
-is_sequence <- function(x, sort = TRUE, ...) UseMethod("is_sequence")
+is_linear_sequence <- function(x, ...) UseMethod("is_linear_sequence")
 
-#' @rdname is_sequence
+#' @rdname is_linear_sequence
 #' @param tol numerical tolerance for gaps.
 #' @export
-is_sequence.numeric <- function(x, sort=TRUE, tol = 1e-8, ...){
-  if (sort) x <- sort(x)
-  length(x) <= 1 || {d <- diff(x); all(d - d[1] <= tol)}
+is_linear_sequence.numeric <- function(x, start=NULL, end=NULL, sort=FALSE, tol = 1e-8, ...){
+
+  # TODO edge case where all diffs are 0
+
+  # Edge cases: empty sequence, or length 1 sequence with missing value.  In
+  # those cases, return FALSE when any of start or end is checked, otherwise
+  # return TRUE
+  if ( length(x) == 0 || (length(x) == 1 && is.na(x)) ) 
+    return(is.null(start) && is.null(end))
+
+  # the regular case
+  !anyNA(x) &&
+    (is.null(start) || abs(start - min(x)) <= tol) &&
+    (is.null(end)   || abs(end - max(x))   <= tol) &&
+    ( length(x) <= 1 || { if(sort) x <- sort(x)
+                          d <- diff(x)
+                          all(abs(d - d[1]) <= tol)
+                        })
+
 }
 
-#' @rdname is_sequence
+#' @rdname is_linear_sequence
 #' @export
-is_sequence.Date <- function(x, sort = TRUE, ...){
+is_linear_sequence.Date <- function(x,start=NULL, end=NULL, sort = FALSE, ...){
   if (sort) x <- sort(x)
   length(x) <= 1 || {d <- diff(x); all(d == d[1])}
 }
 
-#' @rdname is_sequence
+#' @rdname is_linear_sequence
 #' @export
-is_sequence.POSIXct <- function(x, sort = TRUE, ...){
-  is_sequence.Date(x = x, sort = sort, ...)
+is_linear_sequence.POSIXct <- function(x, start=NULL, end=NULL, sort = FALSE, ...){
+  is_linear_sequence.Date(x = x, start=start, end=end, sort = sort, ...)
 }
 
 
--- pkg/inst/tinytest/test_genericrules.R
+++ pkg/inst/tinytest/test_genericrules.R
@@ -1,35 +1,51 @@
 
 # numbers
-expect_true( is_sequence(0) )
-expect_true( is_sequence(c(0,1)) )
-expect_false( is_sequence(c(pi, exp(1),7)) )
+
+expect_true(is_linear_sequence(numeric(0)))
+expect_true( is_linear_sequence(0) )
+expect_true( is_linear_sequence(c(0,1)) )
+expect_false( is_linear_sequence(c(pi, exp(1),7)) )
+
+expect_false( is_linear_sequence(c(3,4,2,1,5), sort=FALSE) )
+expect_true( is_linear_sequence(c(3,4,2,1,5), sort=TRUE) )
+
+expect_false( is_linear_sequence(c(1,NA,2)) )
+
+expect_true( is_linear_sequence(NA_integer_) )
+expect_false( is_linear_sequence(rep(NA_integer_,2)) )
+
+expect_false( is_linear_sequence(1:5, start=2))
+expect_false( is_linear_sequence(1:5, end=7))
+expect_false( is_linear_sequence(1:5, start=1, end=6))
+expect_false( is_linear_sequence(1:5, start=2, end=5))
+
 
 # dates
-expect_true( is_sequence(as.Date("2015-12-17")) )
-expect_true( is_sequence( as.Date(c("2015-12-17","2015-12-19")) ) )
-expect_false( is_sequence(as.Date(c("2015-12-17","2015-12-19","2015-12-20"))) )
+expect_true( is_linear_sequence(as.Date("2015-12-17")) )
+expect_true( is_linear_sequence( as.Date(c("2015-12-17","2015-12-19")) ) )
+expect_false( is_linear_sequence(as.Date(c("2015-12-17","2015-12-19","2015-12-20"))) )
 
 # POSIXct
-expect_true( is_sequence(as.POSIXct(as.Date("2015-12-17"))) )
-expect_true( is_sequence( as.POSIXct(as.Date(c("2015-12-17","2015-12-19"))) ) )
-expect_false( is_sequence(as.POSIXct(as.Date(c("2015-12-17","2015-12-19","2015-12-20"))) ) )
+expect_true( is_linear_sequence(as.POSIXct(as.Date("2015-12-17"))) )
+expect_true( is_linear_sequence( as.POSIXct(as.Date(c("2015-12-17","2015-12-19"))) ) )
+expect_false( is_linear_sequence(as.POSIXct(as.Date(c("2015-12-17","2015-12-19","2015-12-20"))) ) )
 
 # in validator context
 
-d <- data.frame(
-    number = c(pi, exp(1), 7) 
-  , date = as.Date(c("2015-12-17","2015-12-19","2015-12-20"))
-  , time = as.POSIXct(as.Date(c("2015-12-17","2015-12-19","2015-12-20")))
-)
-
-rules <- validator(
-    is_sequence(number)
-  , is_sequence(date)
-  , is_sequence(time)
-)
-
-# nothing works 
-expect_false(any(confront(d,rules)))
+#d <- data.frame(
+#    number = c(pi, exp(1), 7) 
+#  , date = as.Date(c("2015-12-17","2015-12-19","2015-12-20"))
+#  , time = as.POSIXct(as.Date(c("2015-12-17","2015-12-19","2015-12-20")))
+#)
+#
+#rules <- validator(
+#    is_linear_sequence(number)
+#  , is_linear_sequence(date)
+#  , is_linear_sequence(time)
+#)
+#
+## nothing works 
+#expect_false(any(confront(d,rules)))
 
 
 
