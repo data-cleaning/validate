57d795fc40c70e8dc1c8a58c8b2ef9d420d15d63
Mark van der Loo <mark.vanderloo@gmail.com>
2020-11-23 17:21:14 +0100

    updates, formatting pdf

---
+++ cookbook/chunk_opts.R
@@ -0,0 +1,7 @@
+if (knitr::is_latex_output()){
+  knitr::opts_chunk$set(comment=NA
+                      , size      = "tiny"
+                      , tidy      = TRUE
+                      , tidy.opts = list(width.cutoff=60))
+  options(width=60)
+}
